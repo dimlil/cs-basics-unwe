61e8d8a58514458ec0daee6e709f2040bfcedce6
DimVasDev <d.v.vassilev@gmail.com>
2024-02-14 23:55:51 +0200

    feat/ added homework 3

---
+++ homework-3/task-1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace task1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Number of elements: ");
+            int lenght = int.Parse(Console.ReadLine());
+            int maxLength = 0;
+            int currentLength = 1;
+            int start = 0;
+            int end = 0;
+            int[] numbers = new int[lenght];
+            for (int i = 0; i < lenght; i++)
+            {
+                Console.Write($"Element {i}: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] + 1 == numbers[i + 1])
+                {
+                    currentLength++;
+                    if (currentLength > maxLength)
+                    {
+                        maxLength = currentLength;
+                        start = i - maxLength + 2;
+                        end = i + 1;
+                    }
+                }
+                else
+                {
+                    currentLength = 1;
+                }
+            }
+
+            Console.Write("The longest sequence of consecutive numbers is: ");
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+    }
+}
---
+++ homework-3/task-3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+namespace task3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Number of elements: ");
+            int lenght = int.Parse(Console.ReadLine());
+            int currentLength = 1;
+            //int[] numbers = { 9, 6, 2, 7, 4, 7, 6, 6, 8, 4 };
+            int[] numbers = new int[lenght];
+
+            for (int i = 0; i < lenght; i++)
+            {
+                Console.Write($"Element {i}: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(numbers);
+            List<int> sortedArr = new List<int>();
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                Boolean isExist = false;
+                for (int j = 0; j < sortedArr.Count; j++)
+                {
+                    if (sortedArr[j] == numbers[i])
+                    {
+                        isExist = true;
+                    }
+                }
+                if (isExist == false)
+                {
+                    sortedArr.Add(numbers[i]);
+                }
+            }
+            int step = sortedArr[1] - sortedArr[0];
+            for (int i = 0; i < sortedArr.Count; i++)
+            {
+                if (sortedArr[i + 1] - sortedArr[i] == step)
+                {
+                    currentLength++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            for (int i = 0; i < currentLength; i++)
+            {
+                Console.Write($"{sortedArr[i]} ");
+            }
+        }
+    }
+}
+ 
